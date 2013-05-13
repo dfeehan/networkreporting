214ea6b9e98bdc7885c6ae5ddf7334169379340a
dennis feehan <dfeehan@princeton.edu>
2013-05-13 19:32:42 -0400

    added tests for get.var

--- inst/tests/test_helpers.r
+++ inst/tests/test_helpers.r
@@ -1,6 +1,24 @@
-## TODO -- write test for df.to.kpvec
+
+
 
 ## TODO -- write test for get.var
+context("helpers - get.var")
+
+gv.tmp <- data.frame('A'=1:5, 'B'=5:1)
+
+expect_that(get.var(gv.tmp, 'A'), equals(1:5))
+expect_that(get.var(gv.tmp, 1), equals(1:5))
+
+expect_that(get.var(gv.tmp, 'B'), equals(5:1))
+expect_that(get.var(gv.tmp, 2), equals(5:1))
+
+expect_that(get.var(gv.tmp, NULL), equals(rep(NA,5)))
+expect_that(get.var(gv.tmp, NULL, default=-1), equals(rep(-1,5)))
+
+expect_that(get.var(gv.tmp, 10), throws_error())
+
+## TODO -- write test for df.to.kpvec
+
 
 ## TODO -- write test for parse_design
 
