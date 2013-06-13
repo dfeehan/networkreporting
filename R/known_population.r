ba1c66e6f3764c046dbe58acfc74f81c8fa2c51c
dennis feehan <dfeehan@princeton.edu>
2013-06-13 16:21:27 -0400

    kp.degree.estimator no longer takes a weights arg

--- R/known_population.r
+++ R/known_population.r
@@ -20,6 +20,10 @@
 ##' compute an estimate of the respondents' degrees using
 ##' the known population method\cr
 ##'
+##' note that this function does not take survey weights, since
+##' these estimates are not for total degree, but just for the
+##' individual degree of each respondent
+##'
 ##' @param survey.data the dataframe with the survey results
 ##' @param known.popns if not NULL, a vector whose entries are the size of the known
 ##'                    populations, and whose names are the variable names in the dataset
@@ -28,11 +32,6 @@
 ##' @param total.popn.size the size of the entire population. if NULL,
 ##'                        this function returns proportions; if not NULL, it
 ##'                        returns absolute numbers (ie, the proportions * total popn size)
-##' @param weights if not NULL, weights to use in computing the estimate. this
-##'                should be the name of the column in the survey.data which has
-##'                the variable with the appropriate weights. these weights
-##'                should be construted so that, eg, the mean of the degrees is
-##'                estimated as (1/n) * \\sum_i {w_i * d_i}
 ##' @param na.rm if TRUE, disregard rows that have any missingness in
 ##'              the known populations; otherwise, use an adjusted estimator
 ##'              to produce those rows' degree estimates
@@ -45,7 +44,6 @@
 kp.degree.estimator <- function(survey.data,
                                 known.popns=NULL,
                                 total.popn.size=NULL,
-                                weights=NULL,
                                 na.rm=FALSE,
                                 verbose=TRUE)
 {
@@ -58,10 +56,6 @@ kp.degree.estimator <- function(survey.data,
                                            survey.data,
                                            verbose=verbose)
 
-  ## weights will default to 1 for everyone, unless the user specified
-  ## a weights variable
-  weights <- get.weights(survey.data, weights)
-  
   if (! na.rm) {
 
     ## use the modified estimator: for each row, use the known
@@ -74,6 +68,7 @@ kp.degree.estimator <- function(survey.data,
     ## mask for missing values: this matrix has the same shape
     ## as kp.dat, but its entries are 1 if the entry in kp.dat is
     ## observed and 0 if missing
+    ## NB: THIS appears to take a (relatively) long time to run...
     miss.mask <- aaply(kp.dat,
                        1,
                        function(row) {
@@ -81,7 +76,8 @@ kp.degree.estimator <- function(survey.data,
                        },
                        .expand=FALSE)
 
-    ind.tot.known <- (rowSums(kp.dat, na.rm=TRUE) * weights)
+    ind.tot.known <- (rowSums(kp.dat, na.rm=TRUE))
+    ## NB: THIS appears to take a (relatively) long time to run...    
     ind.overall.known <- aaply(miss.mask,
                                1,
                                function(row) {
@@ -93,7 +89,7 @@ kp.degree.estimator <- function(survey.data,
   } else {
 
     tot.known <- (rowSums(subset(survey.data,
-                                 select=names(known.popns))) * weights)
+                                 select=names(known.popns))))    
     
     overall.known <- sum(known.popns)
 
