485afc293680479a10d88d49e4ef572fe3560443
dennis feehan <dfeehan@princeton.edu>
2013-08-26 18:01:09 -0400

    now there are two fns, one for each rds bootstrap method (markov chain based and static chain based). i believe both are now working

--- R/variance_estimators.r
+++ R/variance_estimators.r
@@ -400,76 +400,6 @@ srs.bootstrap.sample <- function(survey.data,
 }
 
 #####################################################
-##' rds.bootstrap.sample
-##'
-##' given an RDS dataset, take a bootstrap resample
-##' to be used in variance estimation
-##'
-##' TODO CITATION
-##'
-##' @param survey.data the dataset to use
-##' @param num.reps the number of bootstrap replication samples to draw
-##' @param parallel if TRUE, use parallelization (via \code{plyr})
-##' @param paropts an optional list of arguments passed on to \code{plyr} to control
-##'        details of parallelization
-##' @param ... ignored, but useful because it allows params like
-##\code{survey.design},
-##' which are used in other bootstrap designs, to be passed in without error
-##' @return a list with \code{num.reps} entries. each entry is a dataset which has
-##' at least the variables \code{index} (the row index of the original dataset that
-##' was resampled) and \code{weight.scale} (the factor by which to multiply the
-##' sampling weights in the original dataset).
-##'
-##' @details \code{survey.design} is not needed; it's included as an argument
-##' to make it easier to drop \code{srs.bootstrap.sample} into the place of
-##' other bootstrap functions, which do require information about the survey design
-##' @export
-rds.bootstrap.sample <- function(survey.data,
-                                 num.reps=1,
-                                 parallel=FALSE,
-                                 paropts=NULL,
-                                 ...)
-{
-
-  survey.data$.internal_id <- 1:nrow(survey.data)
-
-  ## algorithm outlined in SOM for Weir et al 2012:
-  ##
-  ## - partition dataset into A and B, where A is
-  ##   respondents who were recruited by people with trait
-  ##   and B is people who were recruited by others without trait
-  ##
-  ## for each iteration:
-  ##    - pick a bootstrap seed
-  ##    - set current respondent to seed
-  ##    while bootstrap resample not big enough:
-  ##      if current respondent has trait:
-  ##        - pick next respondent uniformly at random from A
-  ##      else:
-  ##        - pick next respondent uniformly at random from B
-  ##      - set current respondent to next respondent
-  ##
-
-  ## TODO BELOW NOT YET CHANGED
-  res <- llply(1:num.reps,
-               function(rep.idx) {
-
-                 these.samples <- sample(1:nrow(survey.data),
-                                         nrow(survey.data),
-                                         replace=TRUE)
-
-                 this.rep <- data.frame(index=these.samples,
-                                        weight.scale=1)
-
-                 return(this.rep)
-               },
-               .parallel=parallel,
-               .paropts=paropts)
-
-  return(res)
-}
-
-#####################################################
 ##' draw RDS bootstrap resamples for one chain
 ##'
 ##' this function uses the algorithm described in the
@@ -552,11 +482,11 @@ rds.boot.draw.chain <- function(chain, mm, dd, parent.trait, idvar="uid") {
 ##' resampled dataset (default is NULL)
 ##' @return a list of length \code{num.reps}; each entry in
 ##' the list has one bootstrap-resampled dataset
-rds.boot.draws <- function(chains,
-                           mm,
-                           dd,
-                           num.reps,
-                           keep.vars=NULL) {
+rds.chain.boot.draws <- function(chains,
+                                 mm,
+                                 dd,
+                                 num.reps,
+                                 keep.vars=NULL) {
 
     traits <- mm$traits
 
