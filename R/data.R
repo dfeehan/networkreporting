7bbe1f8c1cac956de728adaf8bf1b2ae24089b5c
dennis feehan <feehan@berkeley.edu>
2016-04-28 16:11:44 -0700

    fixed docs for example.survey

--- R/data.R
+++ R/data.R
@@ -12,7 +12,36 @@
 ##'   \item{indweight}{the individual weight (relative)}
 ##'   \item{sex}{the sex of the respondent}
 ##'   \item{age.cat}{the age category of the respondent}
-##'   \item{...}{all other rows are reported number of connections to various groups}
+##'   \item{widower}{reported connections to widowers}
+##'   \item{nurse.or.doctor }{reported connections to nurses or doctors}
+##'   \item{male.community.health}{reported connections to male community health workers}
+##'   \item{teacher}{reported connections to teachers}
+##'   \item{woman.smoke}{reported connections to women who smoke}
+##'   \item{priest}{reported connections to priests}
+##'   \item{civil.servant }{reported connections to civil servants}
+##'   \item{woman.gave.birth}{reported connections to women who gave birth}
+##'   \item{muslim}{reported connections to Muslims}
+##'   \item{incarcerated}{reported connections to people who are incarcerated}
+##'   \item{judge}{reported connections to people who are judges}
+##'   \item{man.divorced}{reported connections to men who are divorced}
+##'   \item{treatedfortb}{reported connections to men who are divorced}
+##'   \item{nsengimana}{reported connections to Nsegimanas}
+##'   \item{murekatete}{reported connections to Murekatetes}
+##'   \item{twahirwa}{reported connections to Twahirwas}
+##'   \item{mukandekezi}{reported connections to Mukandekezis}
+##'   \item{nsabimana}{reported connections to Nsabimanas}
+##'   \item{mukamana}{reported connections to Mukamanas}
+##'   \item{ndayambaje}{reported connections to Ndayambajes}
+##'   \item{nyiraneza}{reported connections to Nyiranezas}
+##'   \item{bizimana}{reported connections to Bizimanas}
+##'   \item{nyirahabimana}{reported connections to Nyirahabimanas}
+##'   \item{ndagijimana}{reported connections to Ndagijimanas}
+##'   \item{mukandayisenga}{reported connections to Mukandayisengas}
+##'   \item{died}{reported connections to people who died}
+##'   \item{sex.workers}{reported connections to sex workers}
+##'   \item{msm}{reported connections to msm}
+##'   \item{idu}{reported connections to injecting drug users}
+##'   \item{clients}{reported connections to people who are clients of sex workers}
 ##' }
 "example.survey"
 
